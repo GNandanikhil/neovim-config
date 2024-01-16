9bf0f1ccc5c65628d87cbdf821fe106632f3da9b
nandanikhil gajulapally <nandanikhil.g@gmail.com>
2024-01-16 14:15:01 +0530

    formatted
    Signed-off-by: nandanikhil gajulapally <nandanikhil.g@gmail.com>

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,15 +1,15 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
---  keys = {
-    --{ "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
---  },
-  config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	--  keys = {
+	--{ "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+	--  },
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+	end,
 }
